e662a01fae5890e6ef3d7009eb9b03693c877527
Bernardo Dornellas Cysneiros Gomes de Amorim <bamorim2@gmail.com>
2018-08-22 11:44:57 -0300

    bump exchema version

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ExchemaStreamData.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:exchema, "~> 0.3.0"},
+      {:exchema, "~> 0.4.0"},
       {:stream_data, "~> 0.4.2"}
     ]
   end
